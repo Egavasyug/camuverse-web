be18555c1796dc8600d5483e92bf2164e0e94b93
Dr. Savage <78974706+Egavasyug@users.noreply.github.com>
2025-09-14 20:15:12 -0400

    fix(types): add JSX typing for appkit-button and include .d.ts in tsconfig

---
+++ src/types/appkit.d.ts
@@ -0,0 +1,7 @@
+/* AppKit web components typings */
+declare namespace JSX {
+  interface IntrinsicElements {
+    'appkit-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+  }
+}
+
