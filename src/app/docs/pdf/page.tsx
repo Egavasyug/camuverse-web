10d380f2b604787a762c836be2584cfb5a5e3262
Dr. Savage <78974706+Egavasyug@users.noreply.github.com>
2025-09-22 23:14:45 -0400

    feat(docs): add /docs/pdf route (embedded PDF)

---
+++ src/app/docs/pdf/page.tsx
@@ -0,0 +1,14 @@
+﻿export const metadata = { title: 'Cammunity DAO LLC Whitepaper' }
+
+export default function WhitepaperPage() {
+  return (
+    <div className="h-[calc(100dvh-100px)]">
+      <object data="/docs/CammunityDAO_Overview.pdf" type="application/pdf" width="100%" height="100%">
+        <p>
+          PDF preview unavailable.{' '}
+          <a className="text-blue-600 underline" href="/docs/CammunityDAO_Overview.pdf">Download the whitepaper</a>.
+        </p>
+      </object>
+    </div>
+  )
+}
